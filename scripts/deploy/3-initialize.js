3fa8ab47c0e30df72fe0ab20dccbfa6d8a585a5a
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-27 13:50:31 +0200

    Deployed TOMB

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0x71b2e2eDfE6E881c126c4fE2cCBa553B42ACFEB9';
+  const strategyAddress = '0x235A76b2747728Ec5CBBc36642dc27308C9189be';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
