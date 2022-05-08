ee2b558f07a6f5a67686f1efcc9b1164826d938b
Magnus Brantheim <m.brantheim@gmail.com>
2022-05-08 09:58:29 +0200

    Upgrade USDC

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,7 +1,6 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyLendingOptimizer');
+  const stratContract = await hre.upgrades.upgradeProxy('0x34E4A4670E26A9BB3DD0Ec0909914C8d95B5B0B1', stratFactory, {
     timeout: 0,
   });
   console.log('Strategy upgraded!');
