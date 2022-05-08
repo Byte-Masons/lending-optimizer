3b23dd9686162dbcfd27702bdc108117a43e7b89
Magnus Brantheim <m.brantheim@gmail.com>
2022-05-08 09:31:31 +0200

    Upgrade Tomb

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,7 +1,6 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyLendingOptimizer');
+  const stratContract = await hre.upgrades.upgradeProxy('0x235A76b2747728Ec5CBBc36642dc27308C9189be', stratFactory, {
     timeout: 0,
   });
   console.log('Strategy upgraded!');
