7f98ae493c9f822e841c5eadcc4cfd9f5bf32586
Magnus Brantheim <m.brantheim@gmail.com>
2022-05-23 10:04:03 +0200

    Use node modules for ERC20 in approve script

--- scripts/approve-vault.js
+++ scripts/approve-vault.js
@@ -1,6 +1,6 @@
 async function main() {
   const vaultAddress = '0xDd957FbBdB549B957A1Db92b88bBA5297D0BbE99';
-  const ERC20 = await ethers.getContractFactory('contracts/ERC20.sol:ERC20');
+  const ERC20 = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
   const wantAddress = '0xB12BFcA5A55806AaF64E99521918A4bf0fC40802';
   const want = await ERC20.attach(wantAddress);
   await want.approve(vaultAddress, ethers.utils.parseEther('100'));
@@ -9,7 +9,7 @@ async function main() {
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
